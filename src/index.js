322a530b69bcbe3a14b2713be3b127c04d649e46
garvit811 <sonigarvit811@gmail.com>
2025-08-29 16:12:14 +0530

    Api to handle errors and responses

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import express from "express";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
+app.on("error", (error) => {
+  console.log("Error :", error);
+  throw error 
+})
+
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listening at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.error("MONGO DB connection failed !!! ", err)
+})
+
