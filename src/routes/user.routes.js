6bc4016e6e4918dd33b41663136c48b5fa7c0b80
garvit811 <sonigarvit811@gmail.com>
2025-09-18 20:23:38 +0530

    Add user account routes for change password, current user retrieval, account updates, avatar, cover image, channel profile, and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -35,4 +35,15 @@ router.route("/login").post(upload.none(), loginUser)
 
 router.route("/logout").post(upload.none(), verifyJWT, logoutUser)
 router.route("/refresh-accessToken").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, upload.none(), changeCurrentPassword)
+router.route("current-user").get(verifyJWT, getCurrentUser)
+router.route("update-account").patch(verifyJWT, upload.none(), updateAccountDetails) // used patch as we are only updating some enteries
+
+router.route("/avatar").post(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/coverImage").post(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
+
 export default router
